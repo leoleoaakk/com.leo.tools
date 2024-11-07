9c9da3ec08afc021eeadf6457967340f5e8cf1e9
leoleoaakk <47969698+leoleoaakk@users.noreply.github.com>
2024-11-07 11:05:42 +0800

    Initial commit

---
+++ Runtime/SceneControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Leo.Tools
+{
+    /// <summary>
+    /// 場景控制，提供載入場景與退出遊戲功能
+    /// </summary>
+    public class SceneControl : MonoBehaviour
+    {
+        /// <summary>
+        /// 載入場景
+        /// </summary>
+        /// <param name="sceneName"></param>
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        /// <summary>
+        /// 退出遊戲
+        /// </summary>
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
